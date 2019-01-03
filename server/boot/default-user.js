22142ff55c9ddae33365d54bf90f8545b26d645e
Christian España <espanasilva33@gmail.com>
2019-01-03 01:05:41 -0500

    new models

--- server/boot/default-user.js
+++ server/boot/default-user.js
@@ -1,49 +1,49 @@
-'use strick';
+'use strick'
 
 module.exports = function (app) {
-  const User = app.models.User;
-  const Role = app.models.Role;
-  const RoleMapping = app.models.RoleMapping;
+  const User = app.models.User
+  const Role = app.models.Role
+  const RoleMapping = app.models.RoleMapping
   const userData = {
     username: "admin",
     email: 'admin@example.com',
     password: '*1234#'
-  };
+  }
 
   const roleData = {
     name: 'admin'
-  };
+  }
 
   User.find({ where: { username: 'admin' } }, function (error, user) {
     if (user.length) {
-      return false;
+      return false
     }
 
     User.create(userData, function (error, user) {
       if (error) {
-        throw error;
+        throw error
       }
 
-      console.log(user);
+      console.log(user)
 
       Role.create(roleData, function (error, role) {
         if (error) {
-          throw error;
+          throw error
         }
 
-        console.log(role);
+        console.log(role)
 
         role.principals.create({
           principalType: RoleMapping.USER,
           principalId: user.id
         }, function (error, principal) {
           if (error) {
-            throw error;
+            throw error
           }
 
-          console.log(principal);
-        });
-      });
-    });
-  });
-};
+          console.log(principal)
+        })
+      })
+    })
+  })
+}
